d1c762937a8a6e3f07a3bd4d18177db8531640b3
Ashna Dubey <121333185+Ashna8378@users.noreply.github.com>
2024-07-16 11:33:16 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,38 +1,31 @@
-// Add an event listener to the search button
-
 document.getElementById('search-btn').addEventListener('click', function() {
-    // from here we are getting value from the city input field
     let city = document.getElementById('city-input').value;
-    // here we are checking that input is empty or not 
+    // Here we are checking if the input is empty or not
     if (city) {
-
         getWeatherData(city);
     } else {
-        alert("Please enter a city name");  
+        alert("Please enter a city name");
     }
 });
 
-// here we have used this function to fetch wheather data for the given city
-
-function getWeatherData(city) {
-    let xhr = new XMLHttpRequest();  //here we are creating xml httprequest 
-    let apiKey = 'a89fee4e8304fdb8230088bc61b6fe4b'; // Replace with your OpenWeatherMap API key
-    let url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${apiKey}`;
-
-    xhr.open('GET', url, true);   // here we have initialize get request 
-    xhr.onreadystatechange = function() {
-
-            if (xhr.status == 200) {   // here we are checking request is successful or not 
-                let data = JSON.parse(xhr.responseText);
-                updateWeatherInfo(data);
-            } else {
-                alert("Could not retrieve weather data. Please try again.");
-            }
+async function getWeatherData(city) {
+    try {
+        let apiKey = 'a89fee4e8304fdb8230088bc61b6fe4b';
+        let url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${apiKey}`;
         
-    };
-    xhr.send();  // here we are sending the request to the api
+        let response = await fetch(url);
+        
+        if (response.ok) {
+            let data = await response.json();
+            updateWeatherInfo(data);
+        } else {
+            alert("Could not retrieve weather data. Please try again.");
+        }
+    } catch (error) {
+        alert("An error occurred while fetching the weather data. Please try again.");
+    }
 }
-// this function we have used for updating the weather info on the page 
+
 function updateWeatherInfo(data) {
     let cityName = data.name;
     let temperature = data.main.temp;
@@ -43,13 +36,9 @@ function updateWeatherInfo(data) {
     document.getElementById('temperature').innerText = `Temperature: ${temperature}°C`;
     document.getElementById('weather-description').innerText = `Weather: ${weatherDescription}`;
     document.getElementById('weather-main').innerText = `Condition: ${getWeatherConditionLabel(weatherMain)}`;
-
-
 }
 
-
 function getWeatherConditionLabel(condition) {
-
     if (condition.includes('clear')) {
         return 'Clear';
     } else if (condition.includes('rain')) {
